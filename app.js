6856a6f35f9744b53ea917fffe230ab1cd2dbffa
Mayank Sethi <mayank.sethi1@watchhabitat.com>
2019-12-02 19:27:13 +0530

    added comments to controllers

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+// BUDGET CONTROLLER
 var budgetController = (function(){
     // Some code
 })();
 
+// UI CONTROLLER
 var uiController =(function(){
     // Some code
 })();
 
+// APP CONTROLLER
 var appController = (function(budgetCtrl,uiCtrl){  
    // Some code 
 })(budgetController,uiController);
